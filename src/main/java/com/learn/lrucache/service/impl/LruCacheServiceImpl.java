237cf4ba24b229a7e2465b683e67f43634b4842d
huangxuchu <huangxuchu@yiwafa.com>
2021-01-12 18:34:38 +0800

    增加测试数据

--- src/main/java/com/learn/lrucache/service/impl/LruCacheServiceImpl.java
+++ src/main/java/com/learn/lrucache/service/impl/LruCacheServiceImpl.java
@@ -3,6 +3,12 @@ package com.learn.lrucache.service.impl;
 import com.learn.lrucache.bean.HashLruCache;
 import com.learn.lrucache.service.LruCacheService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /**
  * @author Huangxuchu
@@ -11,10 +17,42 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LruCacheServiceImpl implements LruCacheService {
+    public static final String AREA_PATH = "json/area.json";
+
     private HashLruCache<String, Object> hashLruCache = new HashLruCache<>(30);
 
     @Override
     public String getData() {
         return null;
+    }
+
+    /**
+     * 获取文件内容
+     *
+     * @return
+     * @throws Exception
+     */
+    private String getJson() throws Exception {
+        File file = ResourceUtils.getFile(AREA_PATH);
+        InputStreamReader read = new InputStreamReader(new FileInputStream(file), "utf-8");
+        try {
+            //文件流是否存在； 不存在抛出异常信息
+            if (file.isFile() && file.exists()) {
+                BufferedReader bufferedReader = new BufferedReader(read);
+                StringBuffer stringBuffer = new StringBuffer();
+                String txt = null;
+                //读取文件，将文件内容放入到set中
+                while ((txt = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(txt);
+                }
+                return stringBuffer.toString();
+            } else {
+                throw new Exception("地区文件不存在！");
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            read.close();     //关闭文件流
+        }
     }
 }
